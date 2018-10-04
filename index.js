6ff61c471df7cdfe35cc2a2f180f97f4446d9791
Justin <justindoane@gmail.com>
2018-10-04 14:01:09 -0400

    Transferd data into masterani.me

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Masteranime = require("./main.js");
 require('dotenv').config();
 const client = new Masteranime.Client();
+const fs = require('fs');
 
 
 
@@ -12,18 +13,37 @@ const client = new Masteranime.Client();
 //   }
 // ]
 
+let cypher = {
+  "item watching": 1,
+  "item watched": 2,
+  "item onhold": 3,
+  "item dropped": 4,
+  "item planned": 6
+}
 
+let animeData = fs.readFileSync('list1.json');
+animeData = JSON.parse(animeData);
 client.login(process.env.EMAIL,process.env.PASSWORD).then((User)=>{
-  User.getAnimeEpisodes(2504)
-  .then(episodes=>{
-    episodes.reverse();
-    for(let i=0;i<episodes.length;i++){
-      console.log(`Episode: ${i} - ID: ${episodes[i].info.id}`);
+  (async()=>{
+    for(let i=0;i<animeData.length;i++){
+      console.log(animeData[i].anime_name);
+      animeData[i].anime_name = animeData[i].anime_name.substring(0,30);
+      let anime = await User.searchAnime(animeData[i].anime_name);
+      if(anime[0]){
+      let animeId = anime[0].id;
+      let setStatus = await User.setAnimeStatus(animeId,cypher[animeData[i].anime_status]);
+      if(animeData[i].current_episode){
+        let animeInformation = await User.getAnimeEpisodes(animeId);
+        if(animeInformation[animeData[i].current_episode-1]){
+          let watchedEpisode = await User.watchedEpisode(animeId,animeData[i].current_episode,animeInformation[animeData[i].current_episode-1].info.id);
+        }
+      }
+      }
+      //29
+      //anime[0].id
+
     }
-    //console.log(data.info.title)
-    //console.log(episodes);
-  })
-  .catch(console.error);
+  })();
 }).catch();
 
 
--- main.js
+++ main.js
@@ -25,7 +25,7 @@ class BasicAPI {
   }
   getAnimeEpisodes(animeId){
     return new Promise((resolve,reject)=>{
-      this.getAnime(2504)
+      this.getAnime(animeId)
       .then(data=>{
         let episodes = data.episodes;
         resolve(episodes);
